0a002ae50dade040eb0a68f6db62afb1f0f6af92
Dror Kronstein <dkronst@gmail.com>
2023-09-05 10:56:45 +0300

    fix: incorrect reinstantiation

--- juiced/src/main.rs
+++ juiced/src/main.rs
@@ -8,6 +8,5 @@ fn main() {
     let (fault_tx, fault_rx) = bounded(1);
     let (pilot_tx, pilot_rx) = bounded(1);
     
-    let evse = EVSEHardwareImpl::new();
     start_machine(pilot_rx, fault_rx, &mut evse);
 }
